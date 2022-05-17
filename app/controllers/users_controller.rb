bdf259c8fae6c527ca09d9b3c3036101ab275b89
EC2 Default User <ec2-user@ip-172-31-92-60.ec2.internal>
2022-05-17 14:54:42 +0000

    [topページaboutページ作成完了]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  #users_path /users
+  def index
+  end
+  #/users/:id
+  def show
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  root to: "homes#top"
+  get "homes/about" => "homes#about", as: "about"
+  resources :users, only: [:index, :show, :create,]
+  resources :books, only: [:edit, :index, :show, :update, :create, :destroy]
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
